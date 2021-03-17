93b0c186503e67075689af9b44ea4432fc5940fd
unknown <shaksia13@gmail.com>
2021-03-18 00:58:58 +0200

    Report Model Is Updated Now and it has Delete And Update Report

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -56,13 +56,37 @@ class ReportController extends Controller
     }
 
    
-    public function update(Request $request, $id)
+    public function update(ReportRequest $request,$marker,Report $report)
     {
-        //
+        // [1] Check If This User Is a Owner Or Not
+      if (auth()->id() !== $report->user_id) {
+        return response()->json([
+            'status'=>false,
+            'msg' => 'You can only edit your own marker.'],
+             403);
+      }
+      $request->proof=$request->hasFile('proof') ? $request->file('proof')->store('uploads/proofs','public'): null;
+      $report->update($request->validated());
+      return response()->json([
+        'status'=>true,
+        'msg' => 'تم التعديل بنجاح'],
+         200);
     }
 
-    public function destroy($id)
+    public function destroy($marker,Report $report)
     {
-        //
+        //[1] Check If This User Is a Owner Or Not
+        if (Auth::id() !== $report->user_id) {
+            return response()->json([
+                'status'=>false,
+                'msg' => 'You can only Delete your own report.'],
+                 403);
+          }
+        // [2] Just Delete Report Depend The Report And User Auth
+        $report->delete();
+        return response()->json([
+            'status'=>true,
+            'msg'=>'تم حذف '
+        ]);
     }
 }
